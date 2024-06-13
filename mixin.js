47fedd4e8353918361f351d96261c014f3aab92d
lzk97224 <lzkmm@sina.com>
2024-06-13 12:18:22 +0800

    Create mixin.js

---
+++ mixin.js
@@ -0,0 +1,27 @@
+module.exports.parse = ({ content, name, url }, { yaml, axios, notify }) => {
+  var result = {}
+
+  // 创建XMLHttpRequest对象
+  var xhr = new XMLHttpRequest();
+
+  // 配置同步的GET请求
+  xhr.open('GET', 'https://cdn.jsdelivr.net/gh/Loyalsoldier/clash-rules@release/gfw.txt', false); // 注意这里的false参数，它指定了请求为同步
+
+  // 设置请求完成后的处理函数
+  xhr.onload = function () {
+    if (xhr.status >= 200 && xhr.status < 300) {
+      // 请求成功，处理响应数据
+      console.log(xhr.responseText);
+      result = yaml.parse(xhr.responseText);
+    } else {
+      // 请求失败，处理错误
+      console.error('Request failed with status:', xhr.status);
+    }
+  };
+
+  // 发送请求
+  xhr.send();
+  console.log(result)
+  
+  return content
+}
